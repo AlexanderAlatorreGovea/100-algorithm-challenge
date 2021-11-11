446f24fad919ccb311407d728df01ce692d4d623
Alexander <Alexander_alatorre1993@hotmail.com>
2021-11-10 20:41:19 -0700

    Added testing entities

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -8,5 +8,35 @@ export function htmlEndTagByStartTag(startTag: string): string {
   return closingTag;
 }
 
+const entities = {
+  entities: {
+    assignedTo: {
+      77: {
+        id: "77",
+        name: "Michel Weststrate",
+      },
+      99: {
+        id: "99",
+        name: "Dan Abramov",
+      },
+    },
+    todo: {
+      0: {
+        assignedTo: "99",
+        completed: true,
+        id: "0",
+        name: "create redux",
+      },
+      1: {
+        assignedTo: "77",
+        completed: true,
+        id: "1",
+        name: "create mobx",
+      },
+    },
+  },
+  result: ["0", "1"],
+};
+
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
 console.log(htmlEndTagByStartTag("<i>"));
