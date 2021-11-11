cb5857b36d651e250f1dd01985ec4bd38041ac66
Alexander <Alexander_alatorre1993@hotmail.com>
2021-11-10 23:16:04 -0700

    solved two algorithms

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,33 @@
-export function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
+export function incorrectPasscodeAttempts(
+  passcode: string,
+  attempts: string[]
+): boolean {
+  if (attempts.length > 10) {
+    return false;
+  }
 
+  for (let index = 0; index < attempts.length; index++) {
+      const element = attempts[index];
+      if (passcode === element) {
+          return true;
+        }
+    }
+    
+    return false;
+    //option 2:
+    //return attempts.includes(passcode);
 }
 
-// console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(
+  incorrectPasscodeAttempts("1111", [
+    "1111",
+    "4444",
+    "3333",
+    "8888",
+    "2222",
+    "7777",
+    "0000",
+    "6666",
+    "7285",
+  ])
+);
--- integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
@@ -1,7 +1,20 @@
-export function integerToStringOfFixedWidth(number: number, width: number): string {
+export function integerToStringOfFixedWidth(
+  number: number,
+  width: number
+): string {
+  let stringifiedNumber = number.toString();
+  const lengthOfNumber = stringifiedNumber.length
+  if(width === lengthOfNumber) {
+      return stringifiedNumber
+  }
 
+  if(width < lengthOfNumber) {
+    return  stringifiedNumber.slice(width, lengthOfNumber)
+  }
+
+  return 0 + stringifiedNumber
 }
 
-// console.log(integerToStringOfFixedWidth(1234, 2));
-// console.log(integerToStringOfFixedWidth(1234, 4));
-// console.log(integerToStringOfFixedWidth(1234, 5));
+//console.log(integerToStringOfFixedWidth(1234, 2));
+//console.log(integerToStringOfFixedWidth(1234, 4));
+console.log(integerToStringOfFixedWidth(1234, 5));
