24537cf26a930fe2be73c492f74bbb716038a7a9
Alexander <Alexander_alatorre1993@hotmail.com>
2021-11-14 22:37:31 -0700

    solved another algorithm

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,11 @@
 export function largestNumber(n: number): number {
+  let word = "";
 
+  for (let i = 0; i < n; i++) {
+    word = word.concat("9");
+  }
+
+  return parseInt(word);
 }
 
-// console.log(largestNumber(2));
+console.log(largestNumber(4));
